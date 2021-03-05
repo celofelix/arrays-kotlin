6fa53ca7e92cff7810cb85092356294b162cc91c
Marcelo Felix <celofelix93@gmail.com>
2021-03-05 09:45:26 -0300

    Iterando sobre um IntArray com for e verificando uso do if

--- src/main/kotlin/br/com/alura/arrays/main.kt
+++ src/main/kotlin/br/com/alura/arrays/main.kt
@@ -1,5 +1,35 @@
 package br.com.alura.arrays
 
 fun main() {
-    
+
+    val idades = IntArray(5)
+    idades[0] = 25
+    idades[1] = 19
+    idades[2] = 33
+    idades[3] = 20
+    idades[4] = 55
+
+    var idadeMaior = 0
+    for (idade in idades) {
+        if(idade > idadeMaior)
+            idadeMaior = idade
+    }
+    println("Maior idade com IntArray e For: $idadeMaior")
+
+    var idadeMenor = Int.MAX_VALUE
+    idades.forEach { idade ->
+        if(idade < idadeMenor) {
+            idadeMenor = idade
+        }
+    }
+    println("Menor idade com IntArray e Forach: $idadeMenor")
+
+    val idades2 = intArrayOf(30,72,27,33,55)
+    var maiorIdade = 0
+    idades2.forEach { idade ->
+        if(idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+    println("Maior idade com intArrayOf e ForEach: $maiorIdade")
 }
